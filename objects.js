13fd84ae170e1a3a7ce469964965e72294922176
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 13:21:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,6 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist[artistName] = songTitle;
 };
+function removeFromPlaylist(playlist, artistName){
+  return delete playlist.artistName;
+}
